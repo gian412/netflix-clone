05852c6a648a9572d31b3771fc641dc1f41bd7a7
gian412 <r.gian@protonmail.com>
2021-07-25 23:44:27 +0200

    build: global style

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { GlobalStyles } from './global-style';
 import App from './app';
 
 ReactDOM.render(
     <React.StrictMode>
+        <GlobalStyles />
         <App />
     </React.StrictMode>,
-    document.getElementById('root')
+    document.getElementById('root'),
 );
