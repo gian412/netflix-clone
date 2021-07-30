fd115fd33632872b1fca2751ea4e49481bdd9f7c
gian412 <r.gian@protonmail.com>
2021-07-30 09:04:56 +0200

    build: header for browse page with search box

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link as ReachRouterLink } from 'react-router-dom';
 
-import { Background, Container, Logo, ButtonLink } from './styles/header';
+import {
+    Background,
+    Container,
+    Feature,
+    FeatureCallOut,
+    Group,
+    Text,
+    Link,
+    Logo,
+    ButtonLink,
+    PlayButton,
+    Search,
+    SearchInput,
+    SearchIcon,
+    Profile,
+    Dropdown,
+    Picture,
+} from './styles/header';
 
 interface Props {
     bg?: boolean;
+    src?: string;
+    dontShowOnSmallWiewport?: boolean;
     children?: React.ReactNode;
 }
-
 interface AttributesProps {
     children?: React.ReactNode;
 }
-
 interface LogoProps {
     to: string;
     src: string;
     alt: string;
 }
-
+interface PictureProps {
+    src: string;
+}
+interface LinkProps {
+    active: 'true' | 'false';
+    onClick?: () => void;
+    children?: React.ReactNode;
+}
 interface ButtonProps {
     to: string;
     children?: React.ReactNode;
+}
+interface SearchProps {
+    searchTerm: string;
+    setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const Header = ({ bg = true, children, ...restProps }: Props) => {
@@ -31,6 +59,40 @@ Header.Frame = ({ children, ...restProps }: AttributesProps) => {
     return <Container {...restProps}>{children}</Container>;
 };
 
+Header.Feature = ({ children, ...restProps }: AttributesProps) => {
+    return <Feature {...restProps}>{children}</Feature>;
+};
+
+Header.FeatureCallOut = ({ children, ...restProps }: AttributesProps) => {
+    return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+
+Header.PlayButton = ({ children, ...restProps }: AttributesProps) => {
+    return <PlayButton {...restProps}>{children}</PlayButton>;
+};
+
+Header.Group = ({ children, ...restProps }: AttributesProps) => {
+    return <Group {...restProps}>{children}</Group>;
+};
+
+Header.Search = ({ searchTerm, setSearchTerm, ...restProps }: SearchProps) => {
+    const [searchActive, setSearchActive] = useState(false);
+
+    return (
+        <Search {...restProps}>
+            <SearchIcon onClick={() => setSearchActive(!searchActive)}>
+                <img src="/images/icons/search.png" alt="Search" />
+            </SearchIcon>
+            <SearchInput
+                value={searchTerm}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value)}
+                placeHolder="Search files and series"
+                active={searchActive}
+            />
+        </Search>
+    );
+};
+
 Header.Logo = ({ to, src, alt, ...restProps }: LogoProps) => {
     return (
         <ReachRouterLink to={to}>
@@ -39,12 +101,32 @@ Header.Logo = ({ to, src, alt, ...restProps }: LogoProps) => {
     );
 };
 
+Header.Text = ({ children, ...restProps }: AttributesProps) => {
+    return <Text {...restProps}>{children}</Text>;
+};
+
+Header.Link = ({ children, ...restProps }: LinkProps) => {
+    return <Link {...restProps}>{children}</Link>;
+};
+
 Header.ButtonLink = ({ to, children, ...restProps }: ButtonProps) => {
     return (
         <ButtonLink to={to} {...restProps}>
             {children}
         </ButtonLink>
     );
+};
+
+Header.Profile = ({ children, ...restProps }: AttributesProps) => {
+    return <Profile {...restProps}>{children}</Profile>;
+};
+
+Header.Dropdown = ({ children, ...restProps }: AttributesProps) => {
+    return <Dropdown {...restProps}>{children}</Dropdown>;
+};
+
+Header.Picture = ({ src, ...restProps }: PictureProps) => {
+    return <Picture {...restProps} src={`/images/users/${src}.png`} />;
 };
 
 export default Header;
--- src/containers/browse.tsx
+++ src/containers/browse.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 
 import { Header } from '../components';
 import * as ROUTES from '../constants/routes';
@@ -13,6 +13,11 @@ interface Profile {
 
 export const BrowseContainer = () => {
     const [profile, setProfile] = useState<Profile>({});
+    const [category, setCategory] = useState('series');
+    const [searchTerm, setSearchTerm] = useState('');
+    const [loading, setLoading] = useState(true);
+
+    const { firebase } = useContext(FirebaseContext);
 
     const user = {
         displayName: 'Karl',
@@ -21,7 +26,51 @@ export const BrowseContainer = () => {
 
     return profile.displayName ? (
         <>
-            <p>Browse Container</p>
+            <Header src="joker1" dontShowOnSmallWiewport>
+                <Header.Frame>
+                    <Header.Group>
+                        <Header.Logo to={ROUTES.HOME} src="/images/misc/logo.svg" alt="Netflix" />
+                        <Header.Link
+                            active={category === 'series' ? 'true' : 'false'}
+                            onClick={() => setCategory('series')}
+                        >
+                            Series
+                        </Header.Link>
+                        <Header.Link
+                            active={category === 'films' ? 'true' : 'false'}
+                            onClick={() => setCategory('films')}
+                        >
+                            Films
+                        </Header.Link>
+                    </Header.Group>
+                    <Header.Group>
+                        <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                        <Header.Profile>
+                            <Header.Picture src={user.photoURL} />
+                            <Header.Dropdown>
+                                <Header.Group>
+                                    <Header.Picture src={user.photoURL} />
+                                    <Header.Link active="true">{user.displayName}</Header.Link>
+                                </Header.Group>
+                                <Header.Group>
+                                    <Header.Link active="true" onClick={() => firebase.auth().signOut()}>
+                                        Sign out
+                                    </Header.Link>
+                                </Header.Group>
+                            </Header.Dropdown>
+                        </Header.Profile>
+                    </Header.Group>
+                </Header.Frame>
+                <Header.Feature>
+                    <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+                    <Header.Text>
+                        Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets
+                        of Gotham City. Arthur wears two masks -- the one he paints for his day job as a clown, and the
+                        guise he projects in a futile attempt to feel like he's part of the world around him.
+                    </Header.Text>
+                    <Header.PlayButton>Play</Header.PlayButton>
+                </Header.Feature>
+            </Header>
             <FooterContainer />
         </>
     ) : (
